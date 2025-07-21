4709d6e4539053d670b2ec8c96cdefef5bba873d
pryfmse <135701488+pryfmse@users.noreply.github.com>
2025-07-21 10:40:43 +0300

    Update 7-Reverse-Integer.cpp

--- 1/7-Reverse-Integer.cpp
+++ 1/7-Reverse-Integer.cpp
@@ -14,13 +14,14 @@ int reverse(int x) {
     int res = 0;
     int MAX_INT32 = pow(2, 31) - 1, MIN_INT32 = -pow(2, 31);
     bool flag = false;
+    if (x == MIN_INT32) return 0;
     if (x < 0) {
         flag = !flag;
         x *= -1;
     }
 
     while (x > 0) {
-        if (!(MIN_INT32 <= res * 10 + x % 10 && res * 10 + x % 10 <= MAX_INT32)) {
+        if (!(MIN_INT32 / 10 <= res && res <= (MAX_INT32 - x % 10) / 10)) {
             return 0;
         }
         res = res * 10 + x % 10;
