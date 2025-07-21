25babe4d599e83c7ae45f624eac066cc835c2fe8
pryfmse <135701488+pryfmse@users.noreply.github.com>
2025-07-21 12:42:56 +0300

    Update 32-Longest-Valid-Parentheses.cpp

--- 1/32-Longest-Valid-Parentheses.cpp
+++ 1/32-Longest-Valid-Parentheses.cpp
@@ -6,57 +6,33 @@ using namespace std;
 int longestValidParentheses(string);
 
 int main() {
-    string s = "(()";
+    string s = "(()(((()";
     cout << longestValidParentheses(s) << endl;
 
     s = ")()())";
     cout << longestValidParentheses(s) << endl;
 
-    s = "";
+    s = ")(()(()(((())(((((()()))((((()()(()()())())())()))()()()())(())()()(((()))))()((()))(((())()((()()())((())))(())))())((()())()()((()((())))))((()(((((()((()))(()()(())))((()))()))())";
     cout << longestValidParentheses(s);
 }
 
 int longestValidParentheses(string s) {
-    int n = 0, max_length = 0;
-    forward_list<char> queue;
+    forward_list<int> queue;
+    queue.push_front(-1);
+    int max_res = 0;
 
     for (int i = 0; i < s.size(); i++) {
-        if (s[i] == '{' || s[i] == '(' || s[i] == '[') {
-            queue.push_front(s[i]);
-            n += 1;
-        } else if (!queue.empty()) {
-            if (s[i] == '}') {
-                if (queue.front() != '{') {
-                    max_length = (max_length > n ? max_length : n);
-                    queue.clear();
-                    n = 0;
-                }
-            } else if (s[i] == ')') {
-                if (queue.front() != '(') {
-                    max_length = (max_length > n ? max_length : n);
-                    queue.clear();
-                    n = 0;                }
-            } else {
-                if (queue.front() != '[') {
-                    max_length = (max_length > n ? max_length : n);
-                    queue.clear();
-                    n = 0;                }
-            }
-            queue.pop_front();
-            n += 1;
+        if (s[i] == '(') {
+            queue.push_front(i);
         } else {
-            max_length = (max_length > n ? max_length : n);
-            queue.clear();
-            n = 0;  
+            queue.pop_front();
+            if (queue.empty()) {
+                queue.push_front(i);
+            } else {
+                max_res = max(max_res, i - queue.front());
+            }
         }
     }
 
-    max_length = (max_length > n ? max_length : n);
-
-    while (!queue.empty()) {
-        max_length -= 1;
-        queue.pop_front();
-    }
-
-    return max_length;
+    return max_res;
 }
